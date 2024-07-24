90ac04b7e1e475eabc862ac1c03e2652b5a79abf
Eric_PC_FIXE <edelhaye93@gmail.com>
2024-07-24 15:29:47 +0200

    glowy

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -10,7 +10,7 @@ export default function Home({data}) {
       <div className="main w-dvw h-[85%] flex">
         <div className="left w-1/2 h-full flex flex-col gap-5 justify-center items-start pl-[200px]">
           <h1 className="text-white text-[2rem] ">SO YOU WANNA TRAVEL TO</h1>
-          <h1 className="text-white text-[4rem] ">SPACE</h1>
+          <h1 className="glowbutton text-white text-[4rem] ">SPACE</h1>
           <p className="text-white text-[1rem]">Let's face it; if you want to go to space, you might as well<br></br> genuinely go to outer space and not hover kind of on the<br></br> edge of it. Well sit back, and relax because we'll give you a<br></br>truly out of this world experience!</p>
 
         </div>
